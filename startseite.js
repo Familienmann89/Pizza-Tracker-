1bb81bdab5b4e64cf45539e885c4db2da12ce81d
Ugur452 <ugur.koekser@mnd.thm.de>
2026-08-22 00:52:59 +0200

    Delete startseite.js

--- startseite.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('[data-template]').forEach(button => {
-        button.addEventListener('click', () => {
-            const template = button.dataset.template;
-            window.location.href = `konfigurator.html?template=${encodeURIComponent(template)}`;
-        });
-    });
-});
